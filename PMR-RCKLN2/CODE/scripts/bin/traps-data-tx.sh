f3267ac3c1e896a9756ad179ecde2ade8c98175b
Supreet Singh <supreet.singh@guavus.com>
2015-05-01 23:15:56 +0530

    Pushing final scripts. Freezing MOP scripts, pushed under testing.

--- PMR-RCKLN2/CODE/scripts/bin/traps-data-tx.sh
+++ PMR-RCKLN2/CODE/scripts/bin/traps-data-tx.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Sync data for 24 hours. ALL Alarm files for last 10 days; snmptrapd.log, /var/log/snmptrapd.log.1.gz
 #-------------------------------------------------------------
-source "/opt/guavus/scripts/etc/rckln-pm.cfg"
+cd ~/; source 'guavus/scripts/etc/rckln-pm.cfg'
 if [[ $? -eq '0' ]]; then
-   if [[ `am_i_master` -ne '0' ]]; then exit; fi
+   if [[ `am_i_online` -ne '0' ]]; then exit; fi
 fi
 #-------------------------------------------------------------
 
--- PMR_2/CODE/PMR/bin/SyncPMRHosts.sh
+++ PMR_2/CODE/PMR/bin/SyncPMRHosts.sh
@@ -13,7 +13,7 @@ val=`$RSYNC -v $RSYNCOPT root@${PMRHOST1}:${SAPREPO}/* $BASEPATH/ 2>&1`
 if [[ $? -ne '0' ]]; then
    write_log "$val"; write_log "Sync Failed, Retrying..."
    write_log "Syncing scripts from SAN repository through ${PMRHOST2}"
-   val='';val=`$RSYNC -v $RSYNCOPT root@${PMRHOST2}:${SAPREPO}/* $BASEPATH/ 2>&1 | tr '\n' ' '`
+   val='';val=`$RSYNC -v $RSYNCOPT root@${PMRHOST2}:${SAPREPO}/* $BASEPATH/ 2>&1`
   if [[ $? -eq 0 ]] ; then write_log "$val"; write_log "Sync Complete!" ; else write_log "$val"; write_log "Sync Failed, Exit!" ; fi
 else
 write_log "$val"
